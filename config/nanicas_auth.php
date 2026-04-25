384a11bf55300cfb001b6898ca2c10279f40d934
José Victor <josevictor.jvds@gmail.com>
2026-04-25 18:37:17 -0300

    chore: atualiza dependencias e config/nanicas_auth.php

--- config/nanicas_auth.php
+++ config/nanicas_auth.php
@@ -14,13 +14,18 @@ return [
     'AUTHORIZATION_API_URL' => env('NANICAS_AUTHORIZATION_API_URL'),
     'AUTHORIZATION_PERSONAL_TOKEN' => env('NANICAS_AUTHORIZATION_PERSONAL_TOKEN'),
 
+    'HARD_CONTRACT_ID' => env('NANICAS_HARD_CONTRACT_ID'),
+
     'SESSION_AUTH_KEY' => 'nanicas_auth',
     'SESSION_CLIENT_AUTH_KEY' => 'nanicas_client_auth',
-
+    'AUTHORIZATION_RESPONSE_KEY' => 'authorization_response',
+    'AUTHENTICATION_RESPONSE_KEY' => 'authentication_response',
+    
     'DEFAULT_PERSONAL_TOKEN_MODEL' => Nanicas\Auth\Frameworks\Laravel\Models\PersonalToken::class,
     'DEFAULT_AUTHORIZATION_CLIENT' => Nanicas\Auth\Frameworks\Laravel\Services\ThirdPartyAuthorizationService::class,
     'DEFAULT_AUTHENTICATION_CLIENT' => Nanicas\Auth\Frameworks\Laravel\Services\ThirdPartyAuthenticationService::class,
 
+    'stateless' => false,
     'gate' => [
         'check_acl_permissions' => false,
     ]
